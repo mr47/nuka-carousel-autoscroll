ffdf7948b1f407c669008d888eaa1142538a322f
Dmitry.Poddubniy <mr4700@gmail.com>
2016-02-17 09:17:41 +0200

    updated lib

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 'use strict';
 
 var Decorator = require('./lib/autoScroll');
-var ProgressIcon = require('./lib/autoScrollProgress');
 
 module.exports = Decorator;
-module.exports.ProgressIcon = ProgressIcon;
